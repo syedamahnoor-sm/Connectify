b72cdb55053e12334d5fc5574705f0cff1b7d65b
Syeda Mahnoor <syedamahnoor9e@gmail.com>
2026-05-09 23:19:14 +0500

    Temporarily disable sockets

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,5 +1,9 @@
-import { io } from "socket.io-client";
+// import { io } from "socket.io-client";
 
-const socket = io(import.meta.env.VITE_API_URL); 
+// const socket = io(import.meta.env.VITE_API_URL); 
+
+// export default socket;
+
+const socket = null;
 
 export default socket;
